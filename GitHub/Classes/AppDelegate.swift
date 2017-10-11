6af17c6ad27797d0703796d540381ffae1969c9a
Kevin McKee <kevinmmckee@gmail.com>
2017-10-11 10:40:31 -0700

    Initial commit with bare project layout.

---
+++ GitHub/Classes/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  GitHub
+//
+//  Created by Kevin McKee on 10/11/17.
+//  Copyright © 2017 Procore. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+

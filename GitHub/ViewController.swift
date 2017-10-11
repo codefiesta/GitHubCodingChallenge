d2ff573a2ecdbf86e07b8658309d3d47f2677f91
Kevin McKee <kevinmmckee@gmail.com>
2017-10-11 10:37:38 -0700

    Initial Commit

---
+++ GitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub
+//
+//  Created by Kevin McKee on 10/11/17.
+//  Copyright © 2017 Procore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

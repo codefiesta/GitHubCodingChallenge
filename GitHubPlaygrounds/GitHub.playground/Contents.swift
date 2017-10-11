ecbb6077e8da30e04583ba43de1ceb9efed847bd
Kevin McKee <kevinmmckee@gmail.com>
2017-10-11 14:50:22 -0700

    Have the basics of the API calls working now. Now need to fetch the diff file from the PR.

--- GitHubPlaygrounds/GitHub.playground/Contents.swift
+++ GitHubPlaygrounds/GitHub.playground/Contents.swift
@@ -5,23 +5,127 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-func apiTest() {
-
-    let url = "https://api.github.com/search/users"
-    let params: [String: String] = ["q": "CosmicMind"]
-    request(url, params: params) { (data, error) in
-
-        if let data = data, let string = String(data: data, encoding: String.Encoding.utf8) {
-            print("Received data: \(string)")
-        } else {
-            print("No data received!")
-        }
-        
-        PlaygroundPage.current.finishExecution()
+struct GitHubSearchResults: Codable {
+    
+    var totalCount: Int
+    var users: [GitHubUser]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case users = "items"
     }
 }
 
-func request(_ urlString: String, params: [String: String]? = [:], _ completion: @escaping (Data?, Error?) -> Void) {
+// Represents a User or Organization
+struct GitHubUser: Codable {
+
+    var login: String
+    var avatarUrl: String
+    var reposUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatarUrl = "avatar_url"
+        case reposUrl = "repos_url"
+    }
+}
+
+// Represents a Reop
+struct GitHubRepo: Codable {
+
+    var name: String
+    var description: String
+    var pullsUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case description
+        case pullsUrl = "pulls_url"
+    }
+
+    // Use a custom decoder to cleanup the pulls url
+    // This is done since we can't use the didSet {} observer since it won't fire inside an init() 😢
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        description = try values.decode(String.self, forKey: .description)
+        pullsUrl = try values.decode(String.self, forKey: .pullsUrl).replacingOccurrences(of: "{/number}", with: "")
+    }
+}
+
+// Represents a PR
+struct GitHubPR: Codable {
+    
+    var number: Int
+    var title: String
+    var state: String
+    var diffUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case number
+        case title
+        case state
+        case diffUrl = "diff_url"
+    }
+}
+
+func users() {
+
+    let url = "https://api.github.com/search/users"
+    let params: [String: String] = ["q": "CosmicMind"]
+    
+    request(url, params: params) { (result: GitHubSearchResults?, error) in
+
+        guard let result = result, !result.users.isEmpty else {
+            PlaygroundPage.current.finishExecution()
+        }
+        
+        repos(result.users.first)
+    }
+}
+
+func repos(_ user: GitHubUser?) {
+    
+    guard let user = user else {
+        PlaygroundPage.current.finishExecution()
+    }
+
+    print("🤔 \(user)")
+    request(user.reposUrl) { (results: [GitHubRepo]?, error) in
+
+        guard let results = results, !results.isEmpty else {
+            PlaygroundPage.current.finishExecution()
+        }
+
+        for (index, repo) in results.enumerated() {
+            prs(repo)
+            if index == results.count {
+                PlaygroundPage.current.finishExecution()
+            }
+        }
+        
+    }
+}
+
+func prs(_ repo: GitHubRepo?) {
+    
+    guard let repo = repo else {
+        PlaygroundPage.current.finishExecution()
+    }
+    print("😼 \(repo)")
+    request(repo.pullsUrl) { (results: [GitHubPR]?, error) in
+        guard let results = results else {
+            PlaygroundPage.current.finishExecution()
+        }
+        
+        for result in results {
+            print("🔂 [\(result.number)] \(result.title) => \(result.diffUrl)")
+        }
+    }
+}
+
+
+func request<T: Codable>(_ urlString: String, params: [String: String]? = [:], _ completion: @escaping (T?, Error?) -> Void) {
 
     guard var urlComponents = URLComponents(string: urlString) else {
         return completion(nil, nil)
@@ -53,12 +157,20 @@ func request(_ urlString: String, params: [String: String]? = [:], _ completion:
             return completion(nil, error)
         }
         
-        return completion(data, nil)
+        do {
+            let jsonDecoder = JSONDecoder()
+            let decoded: T = try jsonDecoder.decode(T.self, from: data)
+            return completion(decoded, error)
+            
+        } catch {
+            print("Error: \(error)" )
+            return completion(nil, error)
+        }
+
     })
     
     dataTask.resume()
-
 }
 
-apiTest()
+users()
 
